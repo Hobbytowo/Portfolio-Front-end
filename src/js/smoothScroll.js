a9900926c9363c554a42bf2f3344a4422a4e6cb5
Hobbytowo <weronika.ak@wp.pl>
2018-09-24 02:06:13 +0200

    add smooth scroll down

--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -1,15 +1,31 @@
-const buttonScroll = document.querySelector('.fa-angle-double-up')
+const buttonScrollUp = document.querySelector('.fa-angle-double-up')
+const buttonScrollDown = document.querySelector('.fa-angle-double-down')
 
-const scrollToTop = () => {
-  const scrollTop = document.documentElement.scrollTop
-  if (scrollTop > 0) {
-    window.scrollTo(0, scrollTop - scrollTop / 6)
-    window.requestAnimationFrame(scrollToTop)
+const scrollUp = () => {
+  const scroll = document.documentElement.scrollTop
+  if (scroll > 0) {
+    window.scrollTo(0, scroll - scroll / 6)
+    window.requestAnimationFrame(scrollUp)
   }
 }
 
-buttonScroll.addEventListener('click', e => {
+const scrollDown = () => {
+  const scroll = document.documentElement.scrollTop
+  const scrollTo = 250
+
+  if (scroll < scrollTo) {
+    window.scrollTo(0, scroll + scrollTo / 10)
+    window.requestAnimationFrame(scrollDown)
+  }
+}
+
+buttonScrollUp.addEventListener('click', e => {
   e.preventDefault()
 
-  scrollToTop()
+  scrollUp()
+})
+
+buttonScrollDown.addEventListener('click', () => {
+  console.log('click')
+  scrollDown()
 })
