7dd0a8f52ddca5feb8f30968f82452d26914b1e4
Wer <weronika.ak@wp.pl>
2019-04-11 00:00:30 +0200

    Add colors variables

--- src/js/contact.js
+++ src/js/contact.js
@@ -7,19 +7,19 @@ title.addEventListener('mouseover', () => {
     return
   }
 
-  title.style.opacity = 0
+  title.style.opacity = '0'
   window.setTimeout(() => {
     title.textContent = `Let's code!`
-    title.style.opacity = 1
+    title.style.opacity = '1'
     title.style.color = '#00b740'
   }, 200)
 })
 
 title.addEventListener('mouseout', () => {
-  title.style.opacity = 0
+  title.style.opacity = '0'
   window.setTimeout(() => {
     title.textContent = `Let's talk!`
-    title.style.opacity = 1
+    title.style.opacity = '1'
     title.style.color = '#f7ef5d'
   }, 200)
 })
