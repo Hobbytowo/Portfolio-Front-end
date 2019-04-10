13be93761103ed4a0e9cf4ba0cb02bdbc6fbd9ec
Wer <weronika.ak@wp.pl>
2019-04-10 23:34:51 +0200

    Refactor scroll functions

--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -1,30 +1,21 @@
 const buttonScrollUp = document.querySelector('.fa-angle-double-up')
 const buttonScrollDown = document.querySelector('.fa-angle-double-down')
-
-const scrollUp = () => {
-  const scroll = document.documentElement.scrollTop
-  if (scroll > 0) {
-    window.scrollTo(0, scroll - scroll / 6)
-    window.requestAnimationFrame(scrollUp)
-  }
-}
-
-const scrollDown = () => {
-  const scroll = document.documentElement.scrollTop
-  const scrollTo = 250
-
-  if (scroll < scrollTo) {
-    window.scrollTo(0, scroll + scrollTo / 10)
-    window.requestAnimationFrame(scrollDown)
-  }
-}
+const projectsSection = document.querySelector('.projects')
 
 buttonScrollUp.addEventListener('click', e => {
   e.preventDefault()
 
-  scrollUp()
+  window.scrollBy({
+    top: -window.pageYOffset,
+    behavior: 'smooth'
+  })
 })
 
 buttonScrollDown.addEventListener('click', () => {
-  scrollDown()
+  const heightToScroll = projectsSection.offsetTop - window.pageYOffset
+
+  window.scrollBy({
+    top: heightToScroll,
+    behavior: 'smooth'
+  })
 })
